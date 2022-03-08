47092f0af40b751584d22141c4617e8a6dc7dcc1
Gr3g03 <71180086+Gr3g03@users.noreply.github.com>
2022-03-08 19:05:20 +0100

    .

--- src/05_fetch.js
+++ src/05_fetch.js
@@ -2,7 +2,7 @@
  * Yo, let's practice our API knowledge.
  */
 
-const NicosRepositories = `https://api.github.com/users/minimumviableperson/repos`;
+
 
 // 1. This ^ is an endpoint that returns Nico's github repositories.
 // Write a function called `getNamesOfNicosRepos`. It should:
@@ -10,12 +10,46 @@ const NicosRepositories = `https://api.github.com/users/minimumviableperson/repo
 //  - only keep the .name property of each repository.
 //  - return all Nico's repo names as an array of strings.
 
+const NicosRepositories = `https://api.github.com/users/minimumviableperson/repos`;
+
+
+const repositories = function getNamesOfNicosRepos() {
+    fetch('NicosRepositories')
+        .then(resp => resp.json())
+}
+repositories.name
+
 // 2. Create a JSON server. Add a /posts endpoint to it, which returns a list of posts.
 // A post is: {
 //   id: number,
 //   title: string,
 //   text: string
 // }
+
+
+const posts = [
+    {
+        'id': 1,
+        'title': 'first post',
+        'text': 'texttext'
+    },
+    {
+        'id': 1,
+        'title': 'first post',
+        'text': 'texttext'
+    },
+    {
+        'id': 1,
+        'title': 'first post',
+        'text': 'texttext'
+    }
+]
+
+
+app.get('/posts', (req, res) => {
+    res.send(posts)
+})
+
 // 2.1 Write a function called createPost that:
 // - takes a post as an argument
 // - save your post in the JSON server
